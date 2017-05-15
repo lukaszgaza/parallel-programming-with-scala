d0855877899b692bd4da3806800144677dabb7c7
gazal <lukasz.gaza@ig.com>
2017-05-15 13:15:56 +0200

    Finished implementation of paralle change count and parentheses balancing

--- reductions/src/main/scala/reductions/ParallelParenthesesBalancing.scala
+++ reductions/src/main/scala/reductions/ParallelParenthesesBalancing.scala
@@ -20,7 +20,7 @@ object ParallelParenthesesBalancingRunner {
   def main(args: Array[String]): Unit = {
     val length = 100000000
     val chars = new Array[Char](length)
-    val threshold = 10000
+    val threshold = 2
     val seqtime = standardConfig measure {
       seqResult = ParallelParenthesesBalancing.balance(chars)
     }
@@ -41,25 +41,45 @@ object ParallelParenthesesBalancing {
   /** Returns `true` iff the parentheses in the input `chars` are balanced.
    */
   def balance(chars: Array[Char]): Boolean = {
-    ???
+    def balanceHelper(chars: Array[Char], leftUnbalanced: Int): Boolean =
+    if (leftUnbalanced < 0) false
+    else if (chars.isEmpty) leftUnbalanced == 0
+    else {
+      val newLeftUnbalanced = if (chars.head == '(') leftUnbalanced + 1 else if (chars.head == ')') leftUnbalanced - 1 else leftUnbalanced
+      balanceHelper(chars.tail, newLeftUnbalanced)
+    }
+
+    balanceHelper(chars, 0)
   }
 
   /** Returns `true` iff the parentheses in the input `chars` are balanced.
    */
   def parBalance(chars: Array[Char], threshold: Int): Boolean = {
 
-    def traverse(idx: Int, until: Int, arg1: Int, arg2: Int) /*: ???*/ = {
-      ???
+    def traverse(idx: Int, until: Int, leftUnbalanced: Int, rightUnbalanced: Int): (Int, Int) = {
+      if (idx < until) {
+        chars(idx) match {
+          case '(' => traverse(idx + 1, until, leftUnbalanced + 1, rightUnbalanced)
+          case ')' => if (leftUnbalanced > 0) traverse(idx + 1, until, leftUnbalanced - 1, rightUnbalanced)
+                      else traverse(idx + 1, until, leftUnbalanced, rightUnbalanced + 1)
+          case _ => traverse(idx + 1, until, leftUnbalanced, rightUnbalanced)
+        }
+      } else (leftUnbalanced, rightUnbalanced)
     }
 
-    def reduce(from: Int, until: Int) /*: ???*/ = {
-      ???
+    def reduce(from: Int, until: Int): (Int, Int) = {
+      if (until - from <= threshold) traverse(from, until, 0, 0)
+      else {
+        val mid = (from + until) / 2
+        val (left, right) = parallel(reduce(from, mid), reduce(mid, until))
+        val matching = if (left._1 <= right._2) left._1 else right._2
+        (left._1 + right._1 - matching, left._2 + right._2 - matching)
+      }
     }
 
-    reduce(0, chars.length) == ???
+    reduce(0, chars.length) == (0, 0)
   }
 
   // For those who want more:
   // Prove that your reduction operator is associative!
-
 }
